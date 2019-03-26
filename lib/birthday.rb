4d2d7cebb0e45f35b65e1955b56e1b3a60da44b3
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-26 21:17:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,5 @@ def age_appropriate_birthday(birthday_kids)
     birthday_kids.each do |kids_name,age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
       break if age <= 12
-      
-  end  
+      end  
 end
