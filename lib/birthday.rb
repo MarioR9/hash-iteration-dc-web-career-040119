3bd5579a3677dd5a2507b23b8a922301218cbb25
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-26 21:21:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,9 +12,9 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-    birthday_kids.each do |kids_name,age| 
-      if age == 12 || age < 12
+      maxage = 12
+    birthday_kids.each do |kids_name,age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-      end
-       
+      break if age <= maxage
+      end  
 end
